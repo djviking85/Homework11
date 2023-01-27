4fe2b37d3cb5066ed0596709432b4a271462ad86
Viking <djviking@yandex.ru>
2023-01-27 20:08:39 +0300

    ДЗ 11 с методами, проверяем

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         return year2;
     }
 
+
     public static int proverkaGoda () {
         // этот метод считает високосный или нет и выводит
         var year = yearPrint() ;
